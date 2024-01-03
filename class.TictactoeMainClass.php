2bbee6d8bf65f9364a0090ac342fb41cfd6402e8
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2024-01-03 02:27:09 +0100

    [CHORE] Refactoring code style

--- class.TictactoeMainClass.php
+++ class.TictactoeMainClass.php
@@ -1,261 +1,245 @@
 <?php
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-// Copyright 2020 Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
-
-
-
 /**
-* Tic-tac-toe Main Class
-*
-* @author Riccardo Giovarelli
-* @copyright 2020 Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
-*/
-class TictactoeMainClass {
-
-
+ * Tic-Tac-Toe Main Class
+ *
+ * @author Riccardo Giovarelli
+ */
+class TictactoeMainClass
+{
     /**
-    * Class constructor
-    *
-    * @param    Array   $currentField  Current Tic-tac-toe field
-    */
-    public function __construct($currentField) {
-        
+     * Class constructor
+     *
+     * @param array $current_field Current Tic-Tac-Toe field
+     */
+    public function __construct($current_field)
+    {
         // Init grid
         $cursor = 0;
-        $myGridVector = explode(',', $currentField);
+        $my_grid_vector = explode(",", $current_field);
         for ($i = 0; $i < 3; $i++) {
             for ($j = 0; $j < 3; $j++) {
-                $myFiledMatrix[$i][$j] = intval($myGridVector[$cursor]);
+                $my_filed_matrix[$i][$j] = intval($my_grid_vector[$cursor]);
                 $cursor++;
             }
         }
 
-        // Init property
-        $this->aiMarker = 2;
-        $this->playerMarker = 1;
-        $this->myFieldMatrix = $myFiledMatrix;
+        // Init properties
+        $this->ai_marker = 2;
+        $this->player_marker = 1;
+        $this->my_field_matrix = $my_filed_matrix;
 
-        unset($cursor, $myFiledMatrix, $myGridVector);
+        unset($cursor, $my_filed_matrix, $my_grid_vector);
     }
 
-
-
     /**
-    * Method minimax
-    *
-    * Implement the Minimax algorithm
-    *
-    * @param    Array   $field  Field for the current Tic-tac-toe match
-    * @param    Boolean $isMax  Current turn: maximizer or minimizer
-    * @return   Integer The best rank for the current situation
-    */
-    public function  minimax($field, $isMax) {
-
-        $state = $this->checkCurrentState($field);
+     * @method minimax
+     *
+     * Minimax algorithm implementation
+     *
+     * @param array $field Field for the current Tic-Tac-Toe match
+     * @param boolean $is_max Current turn: maximizer or minimizer
+     * @return integer The best rank for the current step
+     */
+    public function minimax($field, $is_max)
+    {
+        $state = $this->check_current_state($field);
 
         switch ($state) {
-            case '10':
+            case "10":
                 return 10;
-            case '5':
+            case "5":
                 return -10;
-            case '3':
+            case "3":
                 return 0;
         }
-   
 
-        switch ($isMax) {
+        switch ($is_max) {
             case true:
                 $rank = -1000;
                 for ($i = 0; $i < 3; $i++) {
                     for ($j = 0; $j < 3; $j++) {
-                        if ($field[$i][$j] == 0 ) {
-                        $field[$i][$j] = $this->aiMarker;
-                            $rank = max($rank, $this->minimax($field, !$isMax));
+                        if ($field[$i][$j] == 0) {
+                            $field[$i][$j] = $this->ai_marker;
+                            $rank = max(
+                                $rank,
+                                $this->minimax($field, !$is_max)
+                            );
                             $field[$i][$j] = 0;
                         }
                     }
                 }
                 return $rank;
-                break;
             case false:
                 $rank = 1000;
                 for ($i = 0; $i < 3; $i++) {
                     for ($j = 0; $j < 3; $j++) {
-                    if ($field[$i][$j] == 0 ) {
-                            $field[$i][$j] = $this->playerMarker;
-                            $rank = min($rank, $this->minimax($field, !$isMax));
+                        if ($field[$i][$j] == 0) {
+                            $field[$i][$j] = $this->player_marker;
+                            $rank = min(
+                                $rank,
+                                $this->minimax($field, !$is_max)
+                            );
                             $field[$i][$j] = 0;
                         }
                     }
                 }
                 return $rank;
-                break;
             default:
                 return false;
-            }
-   }
-
-
+        }
+    }
 
     /**
-    * Method findBestMove
-    *
-    * Return the best move for AI
-    *
-    * @return   Array   The best move for AI
-    */
-    public function findBestMove() {
-
-
-        $bestVal = -1000;
-        $bestMove = [
+     * @method Method find_best_move
+     *
+     * Return the best move for AI
+     *
+     * @return array The best move for AI
+     */
+    public function find_best_move()
+    {
+        $best_val = -1000;
+        $best_move = [
             "row" => -1,
-            "col" => -1
+            "col" => -1,
         ];
-    
 
         for ($i = 0; $i < 3; $i++) {
             for ($j = 0; $j < 3; $j++) {
-                if ($this->myFieldMatrix[$i][$j] == 0) {
-                    $this->myFieldMatrix[$i][$j] = $this->aiMarker;
-                    $moveVal = $this->minimax($this->myFieldMatrix, false);
-                    $this->myFieldMatrix[$i][$j] = 0;
-                    if ($moveVal > $bestVal) {
-                        $bestMove['row'] = $i;
-                        $bestMove['col'] = $j;
-                        $bestVal = $moveVal;
+                if ($this->my_field_matrix[$i][$j] == 0) {
+                    $this->my_field_matrix[$i][$j] = $this->ai_marker;
+                    $move_val = $this->minimax($this->my_field_matrix, false);
+                    $this->my_field_matrix[$i][$j] = 0;
+                    if ($move_val > $best_val) {
+                        $best_move["row"] = $i;
+                        $best_move["col"] = $j;
+                        $best_val = $move_val;
                     }
                 }
             }
         }
 
-
-        return $bestMove;
-   }
-
-
+        return $best_move;
+    }
 
     /**
-    * Method checkCurrentState
-    *
-    * Check current field state
-    *
-    * @param    Array   $field  Field for the current Tic-tac-toe match
-    * @return   Integer 3 if the match is even, 5 if player wins, 
-    *                   10 if Ai win or 6 if there aren't results
-    */
-    public function checkCurrentState($field) {
-
-
-        $hitMatrix = [
-            'palyer' => [
-                'row' => 0,
-                'column' => 0,
-                'cross' => [
-                    'right' => 0,
-                    'left' => 0
-                ]
+     * @method check_current_state
+     *
+     * Check current field state
+     *
+     * @param array $field Field for the current Tic-tac-toe match
+     * @return integer 3 if the match is even, 5 if player wins, 10 if Ai wins or 6 if there aren't results
+     */
+    public function check_current_state($field)
+    {
+        $hit_matrix = [
+            "palyer" => [
+                "row" => 0,
+                "column" => 0,
+                "cross" => [
+                    "right" => 0,
+                    "left" => 0,
+                ],
             ],
-            'stupidAi' => [
-                'row' => 0,
-                'column' => 0,
-                'cross' => [
-                    'right' => 0,
-                    'left' => 0
-                ]
+            "ai" => [
+                "row" => 0,
+                "column" => 0,
+                "cross" => [
+                    "right" => 0,
+                    "left" => 0,
+                ],
             ],
-            'boxes' => 0
+            "boxes" => 0,
         ];
-
 
         // LOOP LEVEL 1
         for ($i = 0; $i < 3; $i++) {
-
-
             // Rows count reset
-            $hitMatrix['palyer']['row'] = 0;
-            $hitMatrix['stupidAi']['row'] = 0;
+            $hit_matrix["palyer"]["row"] = 0;
+            $hit_matrix["ai"]["row"] = 0;
 
             // Column count reset
-            $hitMatrix['palyer']['column'] = 0;
-            $hitMatrix['stupidAi']['column'] = 0;
+            $hit_matrix["palyer"]["column"] = 0;
+            $hit_matrix["ai"]["column"] = 0;
 
-
-            //Cross win check
+            // Cross win check
             switch ($field[$i][$i]) {
-                case $this->playerMarker:
-                    $hitMatrix['palyer']['cross']['left']++;
+                case $this->player_marker:
+                    $hit_matrix["palyer"]["cross"]["left"]++;
                     break;
-                case $this->aiMarker:
-                    $hitMatrix['stupidAi']['cross']['left']++;
+                case $this->ai_marker:
+                    $hit_matrix["ai"]["cross"]["left"]++;
                     break;
             }
             switch ($field[$i][2 - $i]) {
-                case $this->playerMarker:
-                    $hitMatrix['palyer']['cross']['right']++;
+                case $this->player_marker:
+                    $hit_matrix["palyer"]["cross"]["right"]++;
                     break;
-                case $this->aiMarker:
-                    $hitMatrix['stupidAi']['cross']['right']++;
+                case $this->ai_marker:
+                    $hit_matrix["ai"]["cross"]["right"]++;
                     break;
             }
-            if (($hitMatrix['palyer']['cross']['left'] == 3) || ($hitMatrix['palyer']['cross']['right'] == 3)) return 5;
-            if (($hitMatrix['stupidAi']['cross']['left'] == 3) || ($hitMatrix['stupidAi']['cross']['right'] == 3)) return 10;
-
+            if (
+                $hit_matrix["palyer"]["cross"]["left"] == 3 ||
+                $hit_matrix["palyer"]["cross"]["right"] == 3
+            ) {
+                return 5;
+            }
+            if (
+                $hit_matrix["ai"]["cross"]["left"] == 3 ||
+                $hit_matrix["ai"]["cross"]["right"] == 3
+            ) {
+                return 10;
+            }
 
             // LOOP LEVEL 2
             for ($j = 0; $j < 3; $j++) {
-
-
                 // Rows win check
                 switch ($field[$i][$j]) {
-                    case $this->playerMarker:
-                        $hitMatrix['palyer']['row']++;
+                    case $this->player_marker:
+                        $hit_matrix["palyer"]["row"]++;
                         break;
-                    case $this->aiMarker:
-                        $hitMatrix['stupidAi']['row']++;
+                    case $this->ai_marker:
+                        $hit_matrix["ai"]["row"]++;
                         break;
                 }
-                if ($hitMatrix['palyer']['row'] == 3) return 5;
-                if ($hitMatrix['stupidAi']['row'] == 3) return 10;
-
+                if ($hit_matrix["palyer"]["row"] == 3) {
+                    return 5;
+                }
+                if ($hit_matrix["ai"]["row"] == 3) {
+                    return 10;
+                }
 
                 // Columns win check
                 switch ($field[$j][$i]) {
-                    case $this->playerMarker:
-                        $hitMatrix['palyer']['column']++;
+                    case $this->player_marker:
+                        $hit_matrix["palyer"]["column"]++;
                         break;
-                    case $this->aiMarker:
-                        $hitMatrix['stupidAi']['column']++;
+                    case $this->ai_marker:
+                        $hit_matrix["ai"]["column"]++;
                         break;
                 }
-                if ($hitMatrix['palyer']['column'] == 3) return 5;
-                if ($hitMatrix['stupidAi']['column'] == 3) return 10;
-
+                if ($hit_matrix["palyer"]["column"] == 3) {
+                    return 5;
+                }
+                if ($hit_matrix["ai"]["column"] == 3) {
+                    return 10;
+                }
 
                 // Count boxes
-                if ($field[$i][$j] != 0) $hitMatrix['boxes']++;
+                if ($field[$i][$j] != 0) {
+                    $hit_matrix["boxes"]++;
+                }
             }
         }
 
-
         // Even result check
-        if ($hitMatrix['boxes'] == 9) return 3;
-    
+        if ($hit_matrix["boxes"] == 9) {
+            return 3;
+        }
 
         // No results
         return 6;
-   }
+    }
 }
